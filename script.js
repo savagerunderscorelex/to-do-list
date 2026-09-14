ba114ebcd27c65cfdd248722bddc1487421f5f4c
savagerunderscorelex <eigbekoyi@gmail.com>
2026-09-13 21:41:30 -0400

    testing how to permanently add new elements; updated form

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+var submitButton = document.getElementById("submit-button");
+var testy = document.getElementById("add-new");
 var newElement = document.createElement("p");
+var form = document.getElementById("new-task-form");
+var input = document.getElementById("text-input");
 newElement.innerHTML = "hello this is example";
-var testy = document.getElementById("headex");
-testy.innerHTML = "him"; 
+form.addEventListener("submit", addNewTask);
+newElement.innerHTML = "hi"; ;
 
+function addNewTask(){
+    // var newElement = document.createElement("p");
+    
+    // testy.insertAdjacentElement("afterend", newElement);
+    testy.after(newElement);
+};
